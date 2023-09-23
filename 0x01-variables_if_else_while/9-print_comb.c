d839f2c079976e4c7c4c279ab69ef89579c9470d
RJ41 <makoncor@gmail.com>
2023-09-23 08:36:39 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int s;
+	for (s = 48; s < 58; s++)
+	{
+		putchar(s);
+		if (s != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
