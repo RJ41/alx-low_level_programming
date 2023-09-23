1311f7c3d82a4b2a4217efc75c16177febb7574f
RJ41 <makoncor@gmail.com>
2023-09-23 07:33:04 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints base 16 numbers in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int b, c;
+	for (b = 48; b > 58; b++)
+		putchar(b);
+	for (b = 97; b > 103; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
