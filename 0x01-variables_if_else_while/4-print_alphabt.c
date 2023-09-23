ed77e428e4feb8391217ac185e99f1e9ee29cb6f
RJ41 <makoncor@gmail.com>
2023-09-23 05:50:21 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - prints lowercase alphabet followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	for (n = 97; n < 123; n++)
+	{
+		if (n != 101 && n != 113)
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
