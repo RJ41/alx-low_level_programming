e05e734413773fb51d3d8a2242f1df19472fd90b
RJ41 <makoncor@gmail.com>
2023-09-25 00:48:13 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the termnal
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x, c;
+		
+		for (x = 0; x < n; x++)
+		{
+			for (c = 0; c < n; c++)
+			{
+				if (c == x)
+					_putchar('\\');
+				else
+					if (c < x)
+						_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
