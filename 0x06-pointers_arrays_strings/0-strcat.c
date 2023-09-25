60276532da9548992bb60ef3f9197076946e4480
RJ41 <makoncor@gmail.com>
2023-09-25 13:06:24 -0700

    Task 0: strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int o;
+	int p;
+
+	o = 0;
+	while (dest[o] != '\0')
+		o++;
+	p = 0;
+	while (src[p] != '\0')
+	{
+		dest[o] = src[p];
+		o++;
+		p++;
+	}
+	dest[o] = '\0';
+	return (dest);
+}
