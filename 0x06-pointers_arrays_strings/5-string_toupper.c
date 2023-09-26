63408e25611b44f2274021f47423007727726bcd
RJ41 <makoncor@gmail.com>
2023-09-26 10:52:42 -0700

    TASK 5: Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @n: pointer
+ * Return: 0
+ */
+char *string_toupper(char *n)
+{
+	int d;
+	
+	d = 0;
+	while (n[d] != '\0')
+	{
+		if (n[d] >= 'a' && n[d] <= 'z')
+			n[d] = n[d] - 32;
+	}
+	return (n);
+}
