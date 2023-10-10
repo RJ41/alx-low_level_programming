dd37c4a97339f7ca885dc2c47fe41951b3e496d1
RJ41 <makoncor@gmail.com>
2023-10-10 12:44:42 -0700

    Task 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_x10: prints the alphabet ten times
+ * print_alphabet_x10 - prints the alphabet ten times
+ * followed by new line
+ * Return: 0
  */
 void print_alphabet_x10(void)
 {
