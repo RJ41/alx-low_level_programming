8df22a4fd5d870763af262b076c08541225d45c0
RJ41 <makoncor@gmail.com>
2023-10-17 13:23:08 -0700

    two and three

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,17 @@
 
 /**
  * _strlen - return the lengh of a string
- * @str: string to be printed
+ * @s: string to be printed
  * Return: 0
  */
-size_t _strlen(char *str)
+size_t _strlen(char *s)
 {
 	size_t length = 0;
 	
-	while (*str++)
+	while (*s != '\0')
+	{
 		length++;
+		s++;
+	}
 	return (length);
 }
