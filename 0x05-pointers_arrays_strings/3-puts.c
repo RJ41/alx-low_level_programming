400843da55a5380dfe07105abe20781a87d48e81
RJ41 <makoncor@gmail.com>
2023-10-17 13:04:17 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string output
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *dtr != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
